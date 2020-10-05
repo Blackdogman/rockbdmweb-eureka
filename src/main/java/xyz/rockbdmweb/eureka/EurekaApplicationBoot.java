62794a21913a304ff986411e2aaa3e9791b7929d
Blackdogman <superkidchy@foxmail.com>
2020-10-05 12:54:44 +0800

    update code

--- src/main/java/xyz/rockbdmweb/eureka/EurekaApplicationBoot.java
+++ src/main/java/xyz/rockbdmweb/eureka/EurekaApplicationBoot.java
@@ -1,4 +1,13 @@
 package xyz.rockbdmweb.eureka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplicationBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplicationBoot.class, args);
+    }
 }
